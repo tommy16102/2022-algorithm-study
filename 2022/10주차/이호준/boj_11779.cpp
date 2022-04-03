4f0264b07c58e486721d16b18ba8b3b38620cbf8
hojunlee-hj <cajun7@naver.com>
2022-04-04 00:18:39 +0900

    2022/10주차/Dijkstra/HJ
    commit

--- 2022/10주차/이호준/boj_11779.cpp
+++ 2022/10주차/이호준/boj_11779.cpp
@@ -1,13 +1,11 @@
 /*******************************************************************
 Algorithm Study
 Baekjoon OJ Dijkstra
-1504 특정한 최단 경로
+11779 최소비용 구하기2
 2022/04/03 이호준
 # 아이디어
-1. 다익스트라 알고리즘을 통해서 구현하는 문제.
-정점 2개를 반드시 지난 다는 것은 해당 지점들 까지의 최단거리들을 구해서 구하면 된다는 것이었다.
-그래서 v1을 먼저지나는 경로 1개, v2를 먼저지나는 경로 1개를 통해 빠른 경로를 구하고
-답을 구한다.
+1. 다익스트라 기본문제 및 경로 탐색.
+경로는 다음 next값에 이전 node의 정보를 추가하는 식으로 backtracking한다.
 *******************************************************************/
 #include <iostream>
 #include <vector>
@@ -17,45 +15,54 @@ Baekjoon OJ Dijkstra
 
 using namespace std;
 
-int n, e;
-vector<pair<int,int>> map[801];
-bool cant = false;
+int n, m;
+vector<pair<int,int>> map[1001];
+int trace[1001];
+
 int dijkstra(int start, int end);
 
 int main(void){
 
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> e;
+    cin >> n >> m;
 
-    for(int i = 0 ; i< e;i++){
+    for(int i = 0; i< m; i++){
         int a, b, c;
         cin >> a >> b >> c;
         map[a].push_back({b,c});
-        map[b].push_back({a,c});
     }
 
-    int v1,v2;
-    cin >> v1 >> v2;
+    int start, end;
+    cin >> start >> end;
 
-    long long answer = min(dijkstra(1,v1)+dijkstra(v1,v2)+dijkstra(v2,n),dijkstra(1,v2)+dijkstra(v2,v1)+dijkstra(v1,n));
-
-    if(answer>=INF || cant){
-        cout << -1 ;
-    }else{
-        cout << answer;
+    cout << dijkstra(start,end) <<"\n";
+    vector<int> roots;
+    int current = end;
+    while(1){
+        if(current == start){
+            roots.push_back(start);
+            break;
+        }else{
+            roots.push_back(current);
+            current = trace[current];
+        }
     }
+
+    cout << roots.size() << '\n';
+    for(int i = roots.size()-1; i>=0; i--)
+        cout << roots[i] << " ";
 
     return 0;
 }
 
 int dijkstra(int start, int end){
 
-    // {node, distance}
-    priority_queue<pair<int,int>> q;
     vector<long long> cost(n+1, INF);
 
     cost[start] = 0;
+    // {node, distace}
+    priority_queue<pair<int,int>> q;
     q.push({start, 0});
 
     while(!q.empty()){
@@ -73,12 +80,9 @@ int dijkstra(int start, int end){
             if(next_distance < cost[next_node]){
                 cost[next_node] = next_distance;
                 q.push({next_node, -next_distance});
+                trace[next_node] = current_node;
             }
         }
     }
-    // 못지나가는 경우
-    if(cost[end]==INF)
-        cant = true;
-
     return cost[end];
 }
