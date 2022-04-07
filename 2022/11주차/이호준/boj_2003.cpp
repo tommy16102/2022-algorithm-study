a428750c7b19dd33f274ee911c8600eb7f8c46ff
hojunlee-hj <cajun7@naver.com>
2022-04-07 22:09:31 +0900

    2022/11주차/투포인터/HJ

--- 2022/11주차/이호준/boj_2003.cpp
+++ 2022/11주차/이호준/boj_2003.cpp
@@ -1,3 +1,18 @@
+/*******************************************************************
+Algorithm Study
+Baekjoon OJ Two pointer
+2003 수들의 합 2
+2022/04/05 이호준
+# 아이디어
+1. 왼쪽 오른쪽 포인터 2개를 가지고 알고리즘을 수행하면 된다.
+
+오른쪽 포인터가 끝에 도달한다면 종료시점이다.
+=> 왜냐하면 이전까지 합을 다 했는데도 도달하지 못했거나 도달한 상태이므로
+더이상 체크할 상태가 없는 것이다.
+-> 이점을 간과하고 다시 앞으로 돌아가는 코드를 작성했었다.
+
+투 포인터의 의미를 명확히 알게 되었다.
+*******************************************************************/
 #include <iostream>
 #include <vector>
 
@@ -63,3 +78,48 @@ int main(void){
 
     return 0;
 }
+
+// 투포인터 올바른 풀이
+// 소요시간 : 0MS
+// #include <iostream>
+
+// using namespace std;
+
+// const int MAX = 10000;
+// int arr[MAX];
+
+// int main(void){
+
+//         ios_base::sync_with_stdio(0); cin.tie(0);
+
+//         int N, M;
+
+//         cin >> N >> M;
+
+//         for (int i = 0; i < N; i++)
+//             cin >> arr[i];
+
+//          int low = 0, high = 0; int sum = arr[0]; int result = 0;
+
+//         //투 포인터 알고리즘 적용
+
+//         while(low <= high && high < N){
+
+//                  if (sum < M)
+//                     sum += arr[++high];
+//                 else if (sum == M){
+//                     result++;
+//                     sum += arr[++high];
+//                 }else if (sum > M){
+//                     sum -= arr[low++];
+//                     if (low > high && low < N){
+//                         high = low;
+//                         sum = arr[low];
+//                     }
+//                  }
+//         }
+//         cout << result << "\n";
+
+//         return 0;
+
+// }
