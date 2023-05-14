0cf9efc3c5fcbd82e0693db0f3bd42e1a96d8a58
hojunlee-hj <cajun7@naver.com>
2023-05-14 10:17:49 +0900

    친구네트워크

--- 2023/May/week2/이호준/boj_13422_도둑.cpp
+++ 2023/May/week2/이호준/boj_13422_도둑.cpp
@@ -1,3 +1,11 @@
+/*******************************************************************
+Algorithm Study
+Baekjoon 13422 도둑
+2023/05/14 이호준
+# 아이디어
+1. 슬라이딩 윈도우
+*******************************************************************/
+
 #include <iostream>
 
 using namespace std;
@@ -12,7 +20,7 @@ int main(){
         for(int i = 0; i < n; i++) 
             cin >> a[i];
         int ans=0,sum=0;
-        for(int i = 0; i < M; i++)
+        for(int i = 0; i < m; i++)
             sum+=a[i];
         if(sum < k) 
             ans++;
